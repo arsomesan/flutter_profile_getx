b1728bb255bed73049559a1ed2770d2ea18a0493
Adrian Somesan <som.adrian@gmail.com>
2021-12-04 20:22:40 +0100

    added scroll possibility and settingspage

---
+++ lib/global/glovar.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Glovar {
+  static final grey=Colors.grey;
+  static final white=Colors.white;
+  static final black=Colors.black;
+}
--- lib/screens/ScreenProfil.dart
+++ lib/screens/ScreenProfil.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/src/extension_instance.dart';
+import 'package:get/get_navigation/src/extension_navigation.dart';
 import 'package:profile_try_1/controller/profil_controller.dart';
+import 'package:profile_try_1/screens/ScreenProfileSettings.dart';
 import '../model/profil_model.dart';
+import '../global/glovar.dart';
 
 final ProfilController controller = Get.put(ProfilController());
 Profil profil = controller.profil[0];
@@ -24,9 +27,18 @@ class ScreenProfil extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Profil"),
-        backgroundColor: Colors.grey,
+        backgroundColor: Glovar.grey,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {
+              Get.to(ScreenProfilSettings());
+            }
+          ),
+        ]
       ),
-      body: Column(
+      body: SingleChildScrollView(
+      child: Column(
         children: [
           Container(
             child: Center(
@@ -40,10 +52,6 @@ class ScreenProfil extends StatelessWidget {
                       image: NetworkImage(
                           'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F25%2F49%2F82%2F25498264b4b0e7bd98587789c0e4ffaa.jpg&f=1&nofb=1'),
                       fit: BoxFit.fill),
-                  border: Border.all(
-                      color: Color(0xFFFFFF),
-                      width: 10.0,
-                      style: BorderStyle.solid),
                 ),
               ),
             ),
@@ -104,17 +112,121 @@ class ScreenProfil extends StatelessWidget {
                         ),
                       ),
                     ],
-                  ))),
+                  ),
+              ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 30.0, top: 30.0),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Fähigkeiten",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w900, fontSize: 25)),
+                  Container(
+                    margin: EdgeInsets.only(top: 10.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          "E-Mail: ",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text(email!),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 10.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          "Telefon: ",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text(tel!),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 10.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          "Adresse: ",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text(adresse),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 30.0, top: 30.0),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Fähigkeiten",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w900, fontSize: 25)),
+                  Container(
+                    margin: EdgeInsets.only(top: 10.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          "E-Mail: ",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text(email!),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 10.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          "Telefon: ",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text(tel!),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 10.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          "Adresse: ",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text(adresse),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
+      ),
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.black,
+        backgroundColor: Glovar.black,
         child: Icon(Icons.home),
         onPressed: () {},
       ),
       bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: Colors.grey,
-          selectedItemColor: Colors.white,
+          currentIndex: 1,
+          backgroundColor: Glovar.grey,
+          selectedItemColor: Glovar.white,
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
               icon: Icon(Icons.search),
